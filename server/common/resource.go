c428c4becfae0b8848e9f32878fa37348811a305
hyc <hycsxs@qq.com>
2023-02-24 17:15:25 +0800

    添加静态资源文件路由

---
+++ server/common/resource.go
@@ -0,0 +1,33 @@
+package common
+
+import (
+	"embed"
+	"errors"
+	"io/fs"
+	"path"
+	"path/filepath"
+	"server/resources"
+	"strings"
+)
+
+type Resource struct {
+	fs   embed.FS
+	path string
+}
+
+func NewResource() *Resource {
+	return &Resource{
+		fs:   resources.Static,
+		path: "html",
+	}
+}
+
+func (r *Resource) Open(name string) (fs.File, error) {
+	if filepath.Separator != '/' && strings.ContainsRune(name, filepath.Separator) {
+		return nil, errors.New("http: invalid character in file path")
+	}
+	fullName := filepath.Join(r.path, filepath.FromSlash(path.Clean("/static/"+name)))
+	file, err := r.fs.Open(fullName)
+
+	return file, err
+}
--- server/routes/routes.go
+++ server/routes/routes.go
@@ -50,7 +50,8 @@ func InitRoutes() *gin.Engine {
 	InitMenuRoutes(apiGroup, authMiddleware)         // 注册菜单路由, jwt认证中间件,casbin鉴权中间件
 	InitApiRoutes(apiGroup, authMiddleware)          // 注册接口路由, jwt认证中间件,casbin鉴权中间件
 	InitOperationLogRoutes(apiGroup, authMiddleware) // 注册操作日志路由, jwt认证中间件,casbin鉴权中间件
-
+	// 注册静态资源路由
+	InitStaticRouter(r)
 	common.Log.Info("初始化路由完成！")
 	return r
 }
