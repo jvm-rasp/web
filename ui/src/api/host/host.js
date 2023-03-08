badb247ba55f1269d9fca79e7f82d546b73eb2ec
hyc <hycsxs@qq.com>
2023-03-08 23:42:49 +0800

    更新代码

--- ui/src/api/host/host.js
+++ ui/src/api/host/host.js
@@ -12,6 +12,22 @@ export function deleteHost(params) {
   return request({
     url: '/api/host/delete',
     method: 'post',
+    data: params
+  })
+}
+
+export function updateHost(params) {
+  return request({
+    url: '/api/host/update',
+    method: 'post',
+    data: params
+  })
+}
+
+export function getDetail(params) {
+  return request({
+    url: '/api/host/detail',
+    method: 'get',
     params
   })
 }
--- ui/src/views/host/list/index.vue
+++ ui/src/views/host/list/index.vue
@@ -16,7 +16,7 @@
           </el-select>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">查询</el-button>
+          <el-button type="primary" @click="onSearch">查询</el-button>
           <el-button type="primary" @click="onClear">重置</el-button>
         </el-form-item>
       </el-form>
@@ -42,7 +42,8 @@
         <el-table-column fixed="right" label="操作" align="center">
           <template slot-scope="scope">
             <el-button type="text" size="medium" @click="handleDelete(scope.row.serverID)">删除</el-button>
-            <el-button type="text" size="medium">修改</el-button>
+            <el-button type="text" size="medium" @click="handleEdit(scope.row)">修改</el-button>
+            <el-button type="text" size="medium" @click="handleDetail(scope.row)">详情</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -61,45 +62,42 @@
     <el-dialog title="实例修改" :visible.sync="dialogEditVisible">
       <el-form ref="form" :model="dialogEditForm" label-width="80px">
         <el-form-item label="选择策略">
-          <el-select v-model="dialogEditForm.strategyID" placeholder="请选择活动区域">
-            <el-option label="策略1" value="1000" />
-            <el-option label="策略2" value="2000" />
+          <el-select v-model="dialogEditForm.strategyID" style="width: 100%" placeholder="请选择策略">
+            <el-option
+              v-for="item in configData"
+              :key="item.ID"
+              :label="item.name"
+              :value="item.ID"
+            />
           </el-select>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary">确定</el-button>
-          <el-button>取消</el-button>
+          <el-button type="primary" @click="onUpdate">确定</el-button>
+          <el-button @click="dialogEditVisible=false">取消</el-button>
         </el-form-item>
       </el-form>
     </el-dialog>
     <el-dialog title="实例详情" :visible.sync="dialogDetailVisible">
       <div style="margin: 20px;">
         <el-descriptions title="实例信息" :column="3" border>
-          <el-descriptions-item label="主机名称">kooriookami</el-descriptions-item>
-          <el-descriptions-item label="IP地址">18100000000</el-descriptions-item>
-          <el-descriptions-item label="操作系统">苏州市</el-descriptions-item>
-          <el-descriptions-item label="系统内存">苏州市</el-descriptions-item>
-          <el-descriptions-item label="CPU核数">苏州市</el-descriptions-item>
-          <el-descriptions-item label="可用磁盘">苏州市</el-descriptions-item>
-          <el-descriptions-item label="接入方式">
-            <el-tag size="small">动态</el-tag>
-          </el-descriptions-item>
-          <el-descriptions-item label="在线状态">
-            <el-tag size="small">在线</el-tag>
-          </el-descriptions-item>
-          <el-descriptions-item label="配置生效时间">苏州市</el-descriptions-item>
-          <el-descriptions-item label="注册时间">苏州市</el-descriptions-item>
-          <el-descriptions-item label="备注信息">苏州市</el-descriptions-item>
+          <el-descriptions-item label="主机名称">{{ recordDetail.host.name }}</el-descriptions-item>
+          <el-descriptions-item label="IP地址">{{ recordDetail.host.ip }}</el-descriptions-item>
+          <el-descriptions-item label="操作系统">{{ recordDetail.host.os }}</el-descriptions-item>
+          <el-descriptions-item label="系统内存">{{ recordDetail.host.memory }}</el-descriptions-item>
+          <el-descriptions-item label="CPU核数">{{ recordDetail.host.cpu }}</el-descriptions-item>
+          <el-descriptions-item label="可用磁盘">{{ recordDetail.host.disk }}</el-descriptions-item>
         </el-descriptions>
       </div>
       <div style="margin: 20px;">
-        <el-descriptions title="守护进程信息" :column="3" border>
-          <el-descriptions-item label="运行状态">运行中</el-descriptions-item>
-          <el-descriptions-item label="版本">运行中</el-descriptions-item>
-          <el-descriptions-item label="安装路径">运行中</el-descriptions-item>
-          <el-descriptions-item label="MD5">运行中</el-descriptions-item>
-          <el-descriptions-item label="编译代码">运行中</el-descriptions-item>
-          <el-descriptions-item label="编译时间">运行中</el-descriptions-item>
+        <el-descriptions title="守护进程信息" :column="2" border>
+          <el-descriptions-item label="运行状态">
+            <el-tag size="small" :type="recordDetail.daemon.state === 1 ? 'success':'danger'" disable-transitions>{{ recordDetail.daemon.state === 1 ? '运行中':'离线中' }}</el-tag>
+          </el-descriptions-item>
+          <el-descriptions-item label="版本">{{ recordDetail.daemon.version }}</el-descriptions-item>
+          <el-descriptions-item label="安装路径">{{ recordDetail.daemon.path }}</el-descriptions-item>
+          <el-descriptions-item label="MD5">{{ recordDetail.daemon.md5 }}</el-descriptions-item>
+          <el-descriptions-item label="编译代码">{{ recordDetail.daemon.buildCode }}</el-descriptions-item>
+          <el-descriptions-item label="编译时间">{{ recordDetail.daemon.buildTime }}</el-descriptions-item>
         </el-descriptions>
       </div>
     </el-dialog>
@@ -107,7 +105,8 @@
 </template>
 
 <script>
-import { deleteHost, getHosts } from '@/api/host/host'
+import { deleteHost, getDetail, getHosts, updateHost } from '@/api/host/host'
+import { getConfigs } from '@/api/config/config'
 
 export default {
   data() {
@@ -121,25 +120,64 @@ export default {
       },
 
       tableData: [],
+      configData: [],
       total: 0,
-      dialogEditVisible: true,
+      dialogEditVisible: false,
       dialogDetailVisible: false,
-      dialogEditForm: {}
+      dialogEditForm: {},
+      selectRecord: {},
+      recordDetail: {
+        host: {
+          name: null,
+          ip: null,
+          os: null,
+          memory: null,
+          cpu: null,
+          disk: null
+        },
+        daemon: {
+          state: null,
+          version: null,
+          path: null,
+          md5: null,
+          buildCode: null,
+          buildTime: null
+        }
+      }
     }
   },
   mounted() {
     this.getTableData()
+    this.getConfigData()
   },
   methods: {
-    onSubmit() {
-      console.log('submit!')
+    onSearch() {
       this.getTableData()
     },
+
+    async onUpdate() {
+      const { code, message } = await updateHost(this.dialogEditForm)
+      console.log(code)
+      if (code === 200) {
+        this.$message({
+          type: 'success',
+          message: message
+        })
+        this.dialogEditVisible = false
+      } else {
+        this.$message({
+          type: 'error',
+          message: message
+        })
+      }
+    },
+
     onClear() {
       this.params.name = ''
       this.params.ip = ''
       this.params.method = ''
     },
+
     handlePageSizeChanged(val) {
       this.params.pageSize = val
       this.getTableData()
@@ -149,6 +187,7 @@ export default {
       this.params.pageNum = val
       this.getTableData()
     },
+
     handleDelete(serverID) {
       this.$confirm('您确定要删除该实例吗？删除后不能恢复！', '删除实例', {
         confirmButtonText: '确定',
@@ -175,17 +214,32 @@ export default {
       })
     },
 
+    handleEdit(record) {
+      this.dialogEditForm = record
+      this.dialogEditVisible = true
+    },
+
+    async handleDetail(record) {
+      this.selectRecord = record
+      const { data } = await getDetail(this.params)
+      this.recordDetail = data
+      this.dialogDetailVisible = true
+    },
     // 获取表格数据
     async getTableData() {
       this.loading = true
       try {
         const { data } = await getHosts(this.params)
-        console.log(data.list)
         this.tableData = data.list
         this.total = data.total
       } finally {
         this.loading = false
       }
+    },
+
+    async getConfigData() {
+      const { data } = await getConfigs({})
+      this.configData = data.data
     },
 
     getAgentTypeColor(type) {
