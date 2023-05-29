ac3c7f23a36388c818e8ad494b525a5866f2485c
hyc <hycsxs@qq.com>
2023-05-29 15:52:30 +0800

    修复tls漏洞问题

--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"net/http"
 	"os"
@@ -79,6 +80,27 @@ func main() {
 	go func() {
 		// 服务连接
 		if config.Conf.Ssl.Enable {
+			ciphers := []uint16{
+				tls.TLS_AES_128_GCM_SHA256,
+				tls.TLS_CHACHA20_POLY1305_SHA256,
+				tls.TLS_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+			}
+			tlsConfig := &tls.Config{
+				CipherSuites: ciphers,
+			}
+			srv.TLSConfig = tlsConfig
 			if err := srv.ListenAndServeTLS(config.Conf.Ssl.CertFile, config.Conf.Ssl.KeyFile); err != nil && err != http.ErrServerClosed {
 				common.Log.Fatalf("listen: %s\n", err)
 			}
